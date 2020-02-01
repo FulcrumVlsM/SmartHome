eeacb11346da4f52f12f9536cdc18777c9cebaf4
FulcrumVlsM <misha867458@gmail.com>
2020-02-01 17:27:42 +0300

    Убрал ненужные интерфейсы

---
+++ ServerApps/SmartHomeServer/SmartHome.Data.Store/IDataStore.cs
@@ -0,0 +1,35 @@
+﻿using SmartHome.Data.Models;
+
+namespace SmartHome.Data.Store
+{
+    interface IDataStore
+    {
+        IRepository<BoolActionDevice> BoolActionDevices { get; }
+
+        IRepository<BoolActionDeviceHistory> BoolActionDeviceHistory { get; }
+
+        IRepository<BoolDeviceEventAction> BoolDeviceEventAction { get; }
+
+        IRepository<BoolSensor> BoolSensors { get; }
+
+        IRepository<BoolSensorHistory> BoolSensorHistory { get; }
+
+        IRepository<EventDevice> EventDevices { get; }
+
+        IRepository<EventDeviceHistory> EventDeviceHistory { get; }
+
+        IRepository<NumericSensor> NumericSensors { get; }
+
+        IRepository<NumericSensorHistory> NumericSensorHistory { get; }
+
+        IRepository<Rule> Rules { get; }
+
+        IRepository<RuleNode> RuleNodes { get; }
+
+        IRepository<SmartCard> SmartCards { get; }
+
+        IRepository<User> Users { get; }
+
+        IRepository<UserActionHistory> UserActionHistory { get; }
+    }
+}
