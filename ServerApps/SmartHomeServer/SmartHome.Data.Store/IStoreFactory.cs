1ac03873673bbd562a2a9d5413c9d8b83a02ef24
FulcrumVlsM <misha867458@gmail.com>
2020-02-26 22:49:27 +0300

    #8 Фабрика хранилищ с реализацией под EF Core

---
+++ ServerApps/SmartHomeServer/SmartHome.Data.Store/IStoreFactory.cs
@@ -0,0 +1,23 @@
+﻿namespace SmartHome.Data.Store
+{
+    /// <summary>
+    /// Абстрактная фабрика хранилищ
+    /// </summary>
+    public interface IStoreFactory
+    {
+        /// <summary>
+        /// Хранилище для контроллера
+        /// </summary>
+        IDataStore ControllerDataStore { get; }
+
+        /// <summary>
+        /// Хранилище, предназначенное для конфигурации компонентов
+        /// </summary>
+        IDataStore ConfigurationDataStore { get; }
+
+        /// <summary>
+        /// Хранилище истории работы устройств
+        /// </summary>
+        IHistoryStore HistoryStore { get; }
+    }
+}
---
+++ ServerApps/SmartHomeServer/SmartHome.Data.Store/Stores/EFHistoryStore.cs
@@ -0,0 +1,24 @@
+﻿using SmartHome.Data.Models;
+using SmartHome.Data.EF.Repositories;
+using SmartHome.Data.EF;
+
+namespace SmartHome.Data.Store.Stores
+{
+    internal class EFHistoryStore : IHistoryStore
+    {
+        public IHistoryRepository<BoolActionDeviceHistory> BoolActionDeviceHistory =>
+            new BoolActionDeviceHistoryRepository(new AppDatabaseContext());
+
+        public IHistoryRepository<BoolSensorHistory> BoolSensorHistory =>
+            new BoolSensorHistoryRepository(new AppDatabaseContext());
+
+        public IHistoryRepository<EventDeviceHistory> EventDeviceHistory =>
+            new EventDeviceHistoryRepository(new AppDatabaseContext());
+
+        public IHistoryRepository<NumericSensorHistory> NumericSensorHistory =>
+            new NumericSensorHistoryRepository(new AppDatabaseContext());
+
+        public IHistoryRepository<UserActionHistory> UserActionHistory =>
+            new UserActionHistoryRepository(new AppDatabaseContext());
+    }
+}
