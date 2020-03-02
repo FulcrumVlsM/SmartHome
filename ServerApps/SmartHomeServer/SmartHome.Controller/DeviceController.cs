73e098ae31e7562cb32da8fa63d8cc700a8d29ea
Вилисов Михаил <vilisovam@frte.ru>
2020-03-02 18:38:35 +0300

    #10 Добавил класс контроллера

---
+++ ServerApps/SmartHomeServer/SmartHome.Controller/DeviceController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SmartHome.Controller.Values;
+using SmartHome.Data.Store;
+
+namespace SmartHome.Controller
+{
+    public sealed class DeviceController
+    {
+        private readonly IDataStore _dataStore;
+        private readonly IHistoryStore _historyStore;
+        
+        public DeviceController(IDataStore dataStore, IHistoryStore historyStore)
+        {
+            _dataStore = dataStore;
+            _historyStore = historyStore;
+            //TODO: добавить логгер
+        }
+
+
+        public bool SetBoolSensorValue(BoolSensorValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ThrowEvent(string eventDeviceName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SetNumericSensorValue(NumericSensorValue value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
