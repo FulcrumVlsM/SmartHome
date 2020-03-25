58c0a4ff1b46f21b5c67e2a47a0d8e0bafb5d9a5
Вилисов Михаил <vilisovam@frte.ru>
2020-03-25 16:58:51 +0300

    #14 Подготовка веб-приложения (1)

--- ServerApps/SmartHomeServer/SmartHome.WebApp/Startup.cs
+++ ServerApps/SmartHomeServer/SmartHome.WebApp/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,10 +13,21 @@ namespace SmartHome.WebApp
 {
     public class Startup
     {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly IConfiguration _configuration;
+
+        public Startup(IWebHostEnvironment environment, IConfiguration configuration)
+        {
+            _webHostEnvironment = environment;
+            _configuration = configuration;
+        }
+        
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +42,9 @@ namespace SmartHome.WebApp
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller = Home}/{action=Index}/{id}");
             });
         }
     }
