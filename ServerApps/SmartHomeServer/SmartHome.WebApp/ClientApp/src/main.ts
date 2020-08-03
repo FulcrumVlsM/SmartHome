7593787ac628c8c6d79957871866baa5bcf1fe26
Вилисов Михаил <vilisovam@frte.ru>
2020-08-03 16:00:07 +0300

    Настройка окружения development и production

--- ServerApps/SmartHomeServer/SmartHome.WebApp/ClientApp/src/main.ts
+++ ServerApps/SmartHomeServer/SmartHome.WebApp/ClientApp/src/main.ts
@@ -1,7 +1,15 @@
 ﻿import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment'
 
-enableProdMode();
+if (environment.production) {
+    enableProdMode();
+    console.log("prodmode:enable");
+}
+else {
+    console.log("prodmode:disable");
+}
+
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
